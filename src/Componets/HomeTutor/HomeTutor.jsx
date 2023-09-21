1d590163e85d2f6d727addb674387bb8835ddb21
Belu Gallardo <belugallardo@Mac-mini-de-Emiliano.local>
2023-09-21 13:56:28 -0300

    css botones

--- src/Componets/HomeTutor/HomeTutor.jsx
+++ src/Componets/HomeTutor/HomeTutor.jsx
@@ -10,9 +10,9 @@ const handleClick = () => {
 };
 
 
-const Flecha = <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-arrow-big-left-filled" width="35" height="43" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
-<path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
-<path d="M9.586 4l-6.586 6.586a2 2 0 0 0 0 2.828l6.586 6.586a2 2 0 0 0 2.18 .434l.145 -.068a2 2 0 0 0 1.089 -1.78v-2.586h7a2 2 0 0 0 2 -2v-4l-.005 -.15a2 2 0 0 0 -1.995 -1.85l-7 -.001v-2.585a2 2 0 0 0 -3.414 -1.414z" stroke-width="0" fill="currentColor"></path> </svg>
+// const Flecha = <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-arrow-big-left-filled" width="35" height="43" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+// <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+// <path d="M9.586 4l-6.586 6.586a2 2 0 0 0 0 2.828l6.586 6.586a2 2 0 0 0 2.18 .434l.145 -.068a2 2 0 0 0 1.089 -1.78v-2.586h7a2 2 0 0 0 2 -2v-4l-.005 -.15a2 2 0 0 0 -1.995 -1.85l-7 -.001v-2.585a2 2 0 0 0 -3.414 -1.414z" stroke-width="0" fill="currentColor"></path> </svg>
 
 
 const CerrarSesion = <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-arrow-bar-right" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
@@ -37,10 +37,22 @@ export const HomeTutor = () => {
                     </Link>
                     <CustomButton text="COMUNICACION" onClick={handleClick} estilo={"custom-button"} />
                     <CustomButton text="AJUSTES" onClick={handleClick} estilo={"custom-button"} />
-                    <div className='BotonInferior'>
+                    <div className='contenedorBotonAzul'>
+                    <Link to="/inicio">
+                        <button>
+                            <img className="botonAzul" src="back.png" alt="back" />
+                        </button>
+                    </Link>
+                    <Link to= "/">
+                    <button>
+                        <img className="botonAzul" src="logout.png" alt="accept" />
+                    </button>
+                    </Link>
+                </div>
+                    {/* <div className='BotonInferior'>
                         <CustomButton text= {Flecha} onClick={handleClick} estilo={"custom-buttonAzulRedondo"} />
                         <CustomButton text= {CerrarSesion} onClick={handleClick} estilo={"custom-buttonAzulRedondo"} />
-                    </div>
+                    </div> */}
                 </div>
             </div>
         </>
