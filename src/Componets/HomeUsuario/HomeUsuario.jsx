73b8a49ad1043b048cca95a37e63af75df1ba752
Emiliano Brizuela <emilianobrizuela@Mac-mini-de-Emiliano-2.local>
2023-09-23 18:24:26 -0300

    url2

--- src/Componets/HomeUsuario/HomeUsuario.jsx
+++ src/Componets/HomeUsuario/HomeUsuario.jsx
@@ -1,32 +1,69 @@
-import React , {useEffect} from 'react'
-import { useLocation } from 'react-router-dom'
-import CustomButton from '../Button/Button'
-import '../Button/Button.css'
-import './HomeUsuario.css' 
+// import React , {useEffect} from 'react'
+// import { useLocation } from 'react-router-dom'
+// import CustomButton from '../Button/Button'
+// import '../Button/Button.css'
+// import './HomeUsuario.css' 
+
+// const HomeUsuario = () => {
+//     const handleClick = () => {
+//         alert('Botón personalizado clickeado');
+//     };
+//     const location = useLocation();
+//     useEffect(() => {
+//       const params = new URLSearchParams(location.search);
+//       const esarData = params.get("esarData");
+
+//       console.log(esarData)
+//     },[location]);
+//   return (
+//     <>
+//     <div >
+//         <button className='botonHomeTutor'> Home Tutor </button>
+//     </div>
+//     <div className='homeUsuario'>
+//         <CustomButton text="RUTINA" onClick={handleClick} estilo={"custom-button"} />
+//         <CustomButton text="COMUNICACION" onClick={handleClick} estilo={"custom-button"} />
+//         <CustomButton text="MI CARNET" onClick={handleClick} estilo={"custom-button"} />
+//     </div>
+//     </>
+//   )
+// }
+
+// export default HomeUsuario
+
+import React, { useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
+import CustomButton from '../Button/Button';
+import '../Button/Button.css';
+import './HomeUsuario.css';
 
 const HomeUsuario = () => {
-    const handleClick = () => {
-        alert('Botón personalizado clickeado');
-    };
-    const location = useLocation();
-    useEffect(() => {
-      const params = new URLSearchParams(location.search);
-      const esarData = params.get("esarData");
+  const handleClick = () => {
+    alert('Botón personalizado clickeado');
+  };
+  const location = useLocation();
+  const pathnameParts = location.pathname.split('/'); // Divide la ruta en partes
+  const esarData = pathnameParts[pathnameParts.length - 1]; // Obtén la última parte de la ruta
 
-      console.log("Valor de esarData", esarData)
-    },[location]);
+  useEffect(() => {
+    console.log("Valor de esarData", esarData);
+  }, [location, esarData]);
+
   return (
     <>
-    <div >
+      <div>
         <button className='botonHomeTutor'> Home Tutor </button>
-    </div>
-    <div className='homeUsuario'>
+      </div>
+      <div>
+        <p>{esarData}</p>
+      </div>
+      <div className='homeUsuario'>
         <CustomButton text="RUTINA" onClick={handleClick} estilo={"custom-button"} />
         <CustomButton text="COMUNICACION" onClick={handleClick} estilo={"custom-button"} />
         <CustomButton text="MI CARNET" onClick={handleClick} estilo={"custom-button"} />
-    </div>
+      </div>
     </>
   )
 }
 
-export default HomeUsuario
+export default HomeUsuario;
